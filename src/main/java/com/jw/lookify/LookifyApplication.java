723574213507eed52abd3983b2a308bf14574309
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-09 12:45:51 -0500

    dsf

--- src/main/java/com/jw/lookify/LookifyApplication.java
+++ src/main/java/com/jw/lookify/LookifyApplication.java
@@ -2,9 +2,10 @@ package com.jw.lookify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LookifyApplication {
+public class LookifyApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LookifyApplication.class, args);
